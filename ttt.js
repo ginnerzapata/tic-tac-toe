32f0f985845450333e293123237e80d546b1614d
Ginner Zapata <ginnerzapata@gmail.com>
2020-02-23 00:56:24 -0300

    pushing cell.id to player1.move

--- ttt.js
+++ ttt.js
@@ -1,16 +1,44 @@
+//players
+const Player = (name, mark) =>{
+  let moves = [];
+  let play = cell => moves.push(cell.id);
+  
+  return {name,mark,moves,play}
+}
+
+const player1 = Player('Ginner','X')
+const player2 = ('Lucito', 'O')
+
+
 const displayController = (()=>{
 
 })();
 
+
+
 const gameBoard = (()=>{
-    const cells = document.querySelectorAll('.cell')
-    return{cells}
-})();
+  const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => cell.addEventListener('click',() => {
+    player1.play(cell)
+  }));
+  
 
-const Player = (name, mark) =>{
-    let moves = [];
-    return {name,mark,moves}
-};
+  return {cells, winningCombos};
 
-const player1 = Player('Ginner','X')
-const player2 = ('Lucito', 'O')
+  })();
+
+
+
+
+
+
